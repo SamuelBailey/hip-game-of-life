d1b9651df439f11ebfba08c8e4a94b640a6c3478
Samuel Bailey <samuelrbailey19@gmail.com>
2022-11-08 23:38:17 +0000

    Rework allocations and create stub for kernel function

--- process-life.cu
+++ process-life.cu
@@ -1,15 +1,27 @@
+#include <iostream>
 #include "process-life.h"
 
-Grid::Grid(int x_size, int y_size) {
+__global__
+void GridProc::compute_cell(bool *initial_state, bool *final_state);
 
-}
-
-Grid::Grid(int x_size, int y_size, bool **initial_state) {
+Grid::Grid(int x_size, int y_size, bool *initial_state) {
     void *d_grid_tmp;
-    cudaMallocPitch(&d_grid_tmp, &d_grid_pitch, x_size, y_size);
+    cudaMallocPitch(&d_grid_tmp, &d_grid_pitch, x_size * sizeof(bool), y_size);
     d_grid = static_cast<bool *>(d_grid_tmp);
+
+    cudaMallocPitch(&d_grid_tmp, &d_next_grid_pitch, x_size * sizeof(bool), y_size);
+    d_next_grid = static_cast<bool *>(d_grid_tmp);
+
+    if (initial_state != nullptr) {
+        // TODO: Copy data if not nullptr
+        std::cout << "copied initial state" << std::endl;
+    } else {
+        std::cout << "didn't copy initial state" << std::endl;
+    }
 }
 
 Grid::~Grid() {
     cudaFree(d_grid);
+    cudaFree(d_next_grid);
 }
+
--- process-life.h
+++ process-life.h
@@ -1,11 +1,22 @@
 #pragma once
 
+namespace GridProc {
+
+__global__
+void compute_cell(bool *initial_state, bool *final_state);
+
+} /* namespace GridProc */
+
 class Grid{
 public:
     bool *h_grid;
 private:
     bool *d_grid;
     size_t d_grid_pitch;
+
+    bool *d_next_grid;
+    size_t d_next_grid_pitch;
+
     int x_dim;
     int y_dim;
 
@@ -16,11 +27,11 @@ public:
      * @param x_size 
      * @param y_size 
      */
-    Grid(int x_size, int y_size);
-    Grid(int x_size, int y_size, bool **initial_state);
+    Grid(int x_size, int y_size, bool *initial_state=nullptr);
 
     ~Grid();
 
+public:
     void step_forwards(int n_steps);
     bool *get_host_grid();
     /**
